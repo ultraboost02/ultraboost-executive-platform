c8c47c687e6851748d4c5bb31fea266993ef314c
UltraBoost <tech@ultraboost.pro>
2026-04-03 07:19:25 +0000

    fix: disable no-explicit-any

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-    ],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
